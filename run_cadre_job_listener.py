9170f1140a7f0227b534c85e53402a35fa749830
chathuriw <kamalasini@gmail.com>
2019-08-07 13:19:16 -0400

    run as two processes

--- run_cadre_job_listener.py
+++ run_cadre_job_listener.py
@@ -1,6 +1,10 @@
+from multiprocessing import Process
 import backend.job_listener
 import backend.package_listener
 
 if __name__ == '__main__':
-    backend.job_listener.poll_queue()
-    backend.package_listener.poll_queue()
+    job_process = Process(target=backend.job_listener.poll_queue())
+    job_process.start()
+
+    package_process = Process(target=backend.package_listener.poll_queue())
+    package_process.start()
